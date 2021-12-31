3ddcd803eedf2701c9c7c19b2542c1f704ed04bd
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-31 13:37:45 +0530

    add: added initial scaffold for validating a chain

--- core/blockchain.ts
+++ core/blockchain.ts
@@ -21,6 +21,21 @@ class Blockchain {
 
     return block;
   }
+
+  /**
+   *
+   * @param chain that is to be verified and replaced if valid
+   * @returns whether the chain is valid or not
+   */
+
+  isValidChain(chain: any) {
+    /**
+     * As two objects cant be equal, so we are stringifying the genesis chain and comparing it with the stringified chain
+     */
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+  }
 }
 
 export default Blockchain;
