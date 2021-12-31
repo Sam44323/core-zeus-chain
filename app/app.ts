fadcf996ba84631ce7cb9871b6322396bda1ad6b
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-31 19:34:25 +0530

    add: added server creator and scripts

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,17 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env",
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
