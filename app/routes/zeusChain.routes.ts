ed94862e5999074a428ff13bc6f274a7b57b63ae
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-31 19:59:16 +0530

    add: added controller connection for the routes

--- app/routes/zeusChain.routes.ts
+++ app/routes/zeusChain.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { getBlockData } from "../controllers/zeusChain.controllers";
 
 const zeusChainRoutes = Router();
-zeusChainRoutes.get("/blocks");
+zeusChainRoutes.get("/blocks", getBlockData);
 
 export default zeusChainRoutes;
