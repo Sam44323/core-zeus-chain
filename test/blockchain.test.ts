b17f18d0f46df4b03a3a65c607a7d6239d262919
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-31 14:41:29 +0530

    add: added test for validating a valid chain

--- test/blockchain.test.ts
+++ test/blockchain.test.ts
@@ -3,9 +3,11 @@ import Blockchain from "../core/blockchain";
 
 describe("Blockchain", () => {
   let bChain: Blockchain;
+  let testChain: Blockchain; // chain for testing the validation
 
   beforeEach(() => {
     bChain = new Blockchain();
+    testChain = new Blockchain();
   });
 
   it("starts with the genesis block", () => {
@@ -16,5 +18,10 @@ describe("Blockchain", () => {
     const data = "new-block-data";
     bChain.addBlock(data);
     expect(bChain.chain[bChain.chain.length - 1].data).toEqual(data);
+  });
+
+  it("validates the chain", () => {
+    testChain.addBlock("test-data");
+    expect(bChain.isValidChain(testChain.chain)).toEqual(true);
   });
 });
