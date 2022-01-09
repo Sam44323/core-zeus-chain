114af5ad25982c0351636fe5904e6f6d062ca84c
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-09 15:21:27 +0530

    add: added keypair generation for the wallet

--- utils/chain-util.ts
+++ utils/chain-util.ts
@@ -18,3 +18,4 @@ class ChainUtil {
 }
 
 export default ChainUtil;
+export { EC };
--- wallet/index.ts
+++ wallet/index.ts
@@ -1,13 +1,14 @@
+import ChainUtil, { EC } from "../utils/chain-util";
 import { INITIAL_BALANCE } from "../utils/constants";
 
 class Wallet {
   public balance: number;
-  public keyPair: any;
+  public keyPair: EC.KeyPair;
   public publicKey: any;
   constructor() {
     this.balance = INITIAL_BALANCE;
-    this.keyPair = null;
-    this.publicKey = null;
+    this.keyPair = ChainUtil.genKeyPair();
+    this.publicKey = this.keyPair.getPublic();
   }
 
   toString() {
