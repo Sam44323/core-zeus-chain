9c8055ba263dbc296bfde4a24080801e3ef5dbe1
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-11 23:03:02 +0530

    add: added variable initializer for test for transaction_pool

--- test/transaction_pool.test.ts
+++ test/transaction_pool.test.ts
@@ -1,4 +1,12 @@
 import Transaction from "../wallet/transaction";
 import TransactionPool from "../wallet/transaction_pool";
+import Wallet from "../wallet/index";
 
-describe("Test for transaction-pool", () => {});
+describe("Test for transaction-pool", () => {
+  let transactionPool: TransactionPool,
+    transactions: Transaction,
+    wallet: Wallet,
+    recipient: string,
+    amount: number;
+  beforeEach(() => {});
+});
