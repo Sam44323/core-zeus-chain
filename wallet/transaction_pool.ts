80f5974d108c54ac8691cada2ddbbf6c281ddb79
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-11 22:30:09 +0530

    add: added method for adding or updating a transaction in the list of the transactions that are uncommited

--- wallet/transaction_pool.ts
+++ wallet/transaction_pool.ts
@@ -1,9 +1,27 @@
 import Transaction from "./transaction";
 
 class TransactionPool {
-  public transactions: Transaction[] = [];
+  public transactions: Transaction[] = []; // contains the list of unconfirmed transactions
   constructor() {
     this.transactions = [];
+  }
+
+  /**
+   * method for adding or updating a transaction to the array of transaction
+   * @param transaction the transaction to be added/status-updated to the pool array for confirmation
+   */
+
+  updateOrAddTransaction(transaction: Transaction) {
+    let transactionWithId = this.transactions.find(
+      (t: Transaction) => t.id === transaction.id
+    );
+
+    if (transactionWithId) {
+      this.transactions[this.transactions.indexOf(transactionWithId)] =
+        transaction;
+    } else {
+      this.transactions.push(transaction);
+    }
   }
 }
 
