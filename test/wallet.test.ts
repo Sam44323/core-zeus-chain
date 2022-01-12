05646eb3ddbfbe0d30da325803f4ade6ecb431cf
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-12 22:35:47 +0530

    add: added test initializer for the wallet

---
+++ test/wallet.test.ts
@@ -0,0 +1,5 @@
+import Wallet from "../wallet/index";
+import Transaction from "../wallet/transaction";
+import TransactionPool from "../wallet/transaction_pool";
+
+describe("Test for wallet", () => {});
