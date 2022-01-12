e65a34ef9dec77f98421f172de1f1d0256cb366d
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-12 22:46:24 +0530

    add: added test for checking if the wallet creates a transaction and adds it to the transaction_pool

--- test/wallet.test.ts
+++ test/wallet.test.ts
@@ -1,9 +1,28 @@
 import Wallet from "../wallet/index";
 import TransactionPool from "../wallet/transaction_pool";
+import Transaction from "../wallet/transaction";
 
 describe("Test for wallet", () => {
-  let wallet: Wallet, transactionPool: TransactionPool;
+  let wallet: Wallet,
+    transaction: Transaction,
+    amount: number,
+    recipient: string,
+    transactionPool: TransactionPool;
   beforeEach(() => {
     wallet = new Wallet();
+    transactionPool = new TransactionPool();
+    amount = 49;
+    recipient = "r4nd0m-4ddr355";
+    transaction = wallet.createTransaction(recipient, amount, transactionPool);
+  });
+
+  it("wallet is creating a transaction", () => {
+    expect(
+      JSON.stringify(
+        transactionPool.transactions.find(
+          (transaction: Transaction) => transaction.id
+        )
+      )
+    ).toEqual(JSON.stringify(transaction));
   });
 });
