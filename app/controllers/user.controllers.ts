2ecdd16c34330627774fc4482bfe8e59855410df
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-13 23:42:53 +0530

    add: added file for the user-controllers

---
+++ app/controllers/user.controllers.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+import Wallet from "../../wallet";
+import { wallet } from "../utils/block-initializer";
+
+export const getUserPublicKey = async (req: Request, res: Response) => {};
