808c834d4bfaf9271fe98a76166c71a0b3b36681
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-13 23:40:28 +0530

    add: added file for routes for users

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { chainInitializer } from "./utils/block-initializer";
 import zeusChainRoute from "./routes/zeusChain.routes";
 import transactionRoutes from "./routes/transactions.routes";
+import userRoutes from "./routes/user.routes";
 
 dotenv.config({
   path: ".env",
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/blocks", zeusChainRoute);
 app.use("/transactions", transactionRoutes);
+app.use("/user", userRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
