1c9b63b6bfbd28a8a2cc1b4867e4ea87cefcd0d5
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-13 18:50:46 +0530

    add: added controller connection to the transaction pool

--- app/routes/transactions.routes.ts
+++ app/routes/transactions.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getTransactionsFromPool } from "../controllers/transaction.controllers";
 
 const transactionRoutes = Router();
+
+transactionRoutes.get("/getTransactions", getTransactionsFromPool);
 
 export default transactionRoutes;
