7f84ed6eeaafb94a7e0f5f1e53a0e2253792f954
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-13 23:55:24 +0530

    add: added controller method for getting the key for public for user

--- app/controllers/user.controllers.ts
+++ app/controllers/user.controllers.ts
@@ -2,4 +2,9 @@ import { Request, Response } from "express";
 import Wallet from "../../wallet";
 import { wallet } from "../utils/block-initializer";
 
-export const getUserPublicKey = async (req: Request, res: Response) => {};
+export const getUserPublicKey = async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Public key for the user",
+    publicKey: wallet.publicKey,
+  });
+};
