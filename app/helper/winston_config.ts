52fdf7dd2dd507967d9ccc7a861a0f49c9422697
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-13 19:13:40 +0530

    add: added custom winston logger with updated ignore file

---
+++ app/helper/winston_config.ts
@@ -0,0 +1,20 @@
+var winston = require("winston");
+require("winston-daily-rotate-file");
+
+var transport = new winston.transports.DailyRotateFile({
+  filename: "logger-%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "4d",
+});
+
+transport.on("rotate", function (oldFilename: any, newFilename: any) {
+  // do something fun
+});
+
+var logger = winston.createLogger({
+  transports: [transport],
+});
+
+logger.info("Hello World!");
