87553ee8e5494530d9742a3293b94ac134740b6e
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-14 21:17:59 +0530

    update: updated the data constructs for the miner-class

--- miner/miner.ts
+++ miner/miner.ts
@@ -1,13 +1,20 @@
 import TransactionPool from "../wallet/transaction_pool";
 import Blockchain from "../core/blockchain";
+import Wallet from "../wallet";
 
 class Miner {
-  public transactionPool: TransactionPool;
-  public blockchain: Blockchain;
+  public transactionPool: TransactionPool; // transactionPool instance for the miner
+  public blockchain: Blockchain; // blockchain instance for the miner
+  public wallet: Wallet; // wallet for the miner
 
-  constructor(transactionPool: TransactionPool, blockchain: Blockchain) {
+  constructor(
+    transactionPool: TransactionPool,
+    blockchain: Blockchain,
+    wallet: Wallet
+  ) {
     this.transactionPool = transactionPool;
     this.blockchain = blockchain;
+    this.wallet = Wallet;
   }
 }
 
