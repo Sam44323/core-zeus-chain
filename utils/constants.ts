cdb8ace215176e794d880f777c38f4f5cc1c6db4
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-19 23:51:19 +0530

    add: added mining reward constant value in the config

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,4 @@
 export const DIFFICULTY = 4;
 export const MINE_RATE = 3000;
 export const INITIAL_BALANCE = 500; // we are giving initial balance to something >0 just for testing purposes of this application
+export const MINING_REWARD = 48;
