32d8f0b76136b06ee72bcbbee44b7924ea68ca0c
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-22 17:26:59 +0530

    add: added clearing message types to custom_types

--- utils/types.ts
+++ utils/types.ts
@@ -1,9 +1,11 @@
-export type MESSAGE_OPTION_TYPES = "CHAIN" | "TRANSACTION";
+export type MESSAGE_OPTION_TYPES = "CHAIN" | "TRANSACTION" | "CLEAR";
 export interface MESSAGE_OPTION_INTERFACE {
   chain: MESSAGE_OPTION_TYPES;
   transaction: MESSAGE_OPTION_TYPES;
+  clear: MESSAGE_OPTION_TYPES;
 }
 export const MESSAGE_OPTION: MESSAGE_OPTION_INTERFACE = {
   chain: "CHAIN",
   transaction: "TRANSACTION",
+  clear: "CLEAR",
 };
