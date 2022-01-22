f28c9eff76e30f4c6a511536a1d83bab1e00ea61
Suranjan <suranjanbiswas2002@gmail.com>
2022-01-22 18:00:29 +0530

    add: added new miner initializer for every node

--- app/utils/block-initializer.ts
+++ app/utils/block-initializer.ts
@@ -2,17 +2,20 @@ import Blockchain from "../../core/blockchain";
 import P2PServer from "../../peer-2-peer-server/p2pserver";
 import Wallet from "../../wallet/index";
 import TransactionPool from "../../wallet/transaction_pool";
+import Miner from "../../miner/miner";
 
 let zeusBlockchain: Blockchain;
 let p2pServer: P2PServer;
 let wallet: Wallet;
 let transactionPool: TransactionPool;
+let minerInstance: Miner;
 
 const chainInitializer = () => {
   zeusBlockchain = new Blockchain();
   wallet = new Wallet(); // creating a new wallet for the node
   transactionPool = new TransactionPool();
   p2pServer = new P2PServer(zeusBlockchain, transactionPool);
+  minerInstance = new Miner(transactionPool, zeusBlockchain, wallet, p2pServer);
   p2pServer.listen();
 };
 
